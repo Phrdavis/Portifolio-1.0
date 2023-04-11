36a748429f65db1dd690c8d0180cc49b8765f6a5
Davi Pinheiro de Souza <105741181+Phrdavis@users.noreply.github.com>
2023-04-11 12:51:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-alert("Hello")
+var count = 0;
+
+function mouse_ball(event){
+    var x = event.clientX;
+    var y = event.clientY;
+    var teste = document.getElementById("teste")
+    var teste1 = document.getElementById("teste1")
+    var teste2 = document.getElementById("teste2")
+    var teste3 = document.getElementById("teste3")
+    teste.style.top = `${y}px`;
+    teste.style.left = `${x}px`;
+    
+    teste1.style.top = `${y}px`;
+    teste1.style.left = `${x}px`;
+    
+    teste2.style.top = `${y}px`;
+    teste2.style.left = `${x}px`;
+    
+    teste3.style.top = `${y}px`;
+    teste3.style.left = `${x}px`;
+
+}
+
+function color_ball(event){
+    if(count == 0){
+        document.getElementById("teste").style.backgroundColor = "#c59d19"
+        document.getElementById("teste1").style.backgroundColor = "#c59d19"
+        document.getElementById("teste2").style.backgroundColor = "#c59d19"
+        document.getElementById("teste3").style.backgroundColor = "#c59d19"
+        count = 1
+    }else{
+        document.getElementById("teste").style.backgroundColor = "#B6BFA3"
+        document.getElementById("teste1").style.backgroundColor = "#B6BFA3"
+        document.getElementById("teste2").style.backgroundColor = "#B6BFA3"
+        document.getElementById("teste3").style.backgroundColor = "#B6BFA3"
+        count = 0
+    }
+}
